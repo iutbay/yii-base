9e34f097844cfa0d35f6f3948a2ee359bd14345b
Kevin LEVRON <kevin.levron@univ-pau.fr>
2014-01-13 16:25:41 +0100

    Add custom ActiveRecord class

---
+++ protected/components/ActiveRecord.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * ActiveRecord is the customized base AR class.
+ * All AR model classes for this application should extend from this base class.
+ */
+class ActiveRecord extends CActiveRecord
+{
+
+}
